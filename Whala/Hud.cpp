9b3b674a34219bb79cb6463ce073496876bd7acf
JunkyPonY <gryfenflash@gmail.com>
2017-02-03 21:34:24 +0100

    shit

--- Whala/Hud.cpp
+++ Whala/Hud.cpp
@@ -18,7 +18,7 @@ Hud::Hud(float w, float h)
 void Hud::draw(KlaoudeEngine::SpriteBatch& spriteBatch)
 {
 	m_lifeBarRect = glm::vec4(m_width / 2 - 154, 107, m_player->getHealth()*310/m_player->getMaxHealth(), 11.f);
-	m_zRect = glm::vec4(m_width / 2 - 156, 47, 74, m_player->getzCool() * 52 / m_player->getzCool() - (m_player->getAttackSpeed() - 1.f));
+	m_zRect = glm::vec4(m_width / 2 - 156, 47, 74, m_player->getzCoolCur() * 52 / m_player->getzCoolCur());
 		
 	spriteBatch.draw(m_hudRect, m_uvRect, m_hudTexture, 0.f, c_white);
 
